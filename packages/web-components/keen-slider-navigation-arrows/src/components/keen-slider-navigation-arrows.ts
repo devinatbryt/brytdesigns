a7d2d9568d89a650d10e5c8a6a0d9e959395e1ec
devinatbryt <devin@brytdesigns.com>
2025-04-13 12:27:25 -0700

    Ported web component from other projects

---
+++ packages/web-components/keen-slider-navigation-arrows/src/components/keen-slider-navigation-arrows.ts
@@ -0,0 +1,138 @@
+import type { CorrectComponentType } from "@brytdesigns/web-component-utils";
+
+import html from "solid-js/html";
+import { createEffect, on, onCleanup, Show } from "solid-js";
+import {
+  getKeenSliderContext,
+  type KeenSliderInstance,
+} from "@brytdesigns/web-component-keen-slider";
+
+import {
+  isFirstSlide,
+  updateArrows,
+  hasMoreSlides,
+  addVisibleStyles,
+  addHiddenStyles,
+  isLastSlide,
+  srOnly,
+} from "../utils";
+
+type KeenSliderNavigationArrowsContextProps = {
+  target: string;
+  class: string;
+};
+
+export const KeenSliderNavigationArrows: CorrectComponentType<
+  KeenSliderNavigationArrowsContextProps
+> = (props, { element }) => {
+  if (!props.target)
+    return console.warn(
+      "keen-slider-navigation-arrows: Needs a proper target in order to properly extend a keen slider."
+    );
+  let targetEl: HTMLElement | null = element;
+  if (props.target) targetEl = document.querySelector(props.target);
+  if (!targetEl)
+    return console.warn(
+      "keen-slider-navigation-arrows: Could not find the target element. Make sure it exists and is a keen-slider element."
+    );
+  if (targetEl.tagName !== "KEEN-SLIDER")
+    targetEl = targetEl.querySelector("keen-slider");
+  if (!targetEl)
+    return console.warn(
+      "keen-slider-navigation-arrows: Could not find the target element. Make sure it exists and is a keen-slider element."
+    );
+  const [slider] = getKeenSliderContext(targetEl);
+  const leftArrowTmpl = element.querySelector<HTMLTemplateElement>(
+    "template[prev-arrow]"
+  );
+  const rightArrowTmpl = element.querySelector<HTMLTemplateElement>(
+    "template[next-arrow]"
+  );
+
+  let leftArrow: HTMLElement, rightArrow: HTMLElement;
+
+  createEffect(
+    on(slider, (slider) => {
+      if (!slider) return;
+
+      const handleArrowsUpdate = updateArrows(leftArrow, rightArrow);
+
+      function hideAddIfDisabled(slider: KeenSliderInstance) {
+        if (slider.options.disabled)
+          return element.setAttribute("data-keen-slider-disabled", "true");
+        return element.removeAttribute("data-keen-slider-disabled");
+      }
+
+      function handleUpdate(slider: KeenSliderInstance) {
+        hideAddIfDisabled(slider);
+        handleArrowsUpdate(slider);
+      }
+
+      slider.on("created", handleUpdate);
+      slider.on("slideChanged", handleUpdate);
+      slider.on("optionsChanged", handleUpdate);
+      slider.on("destroyed", handleUpdate);
+
+      return onCleanup(() => {
+        slider.on("created", handleUpdate, true);
+        slider.on("slideChanged", handleUpdate, true);
+        slider.on("optionsChanged", handleUpdate, true);
+        slider.on("destroyed", handleUpdate, true);
+      });
+    })
+  );
+
+  createEffect(
+    on(slider, (slider) => {
+      if (!slider) return;
+
+      if (!hasMoreSlides(slider)) return addHiddenStyles(element);
+      return addVisibleStyles(element);
+    })
+  );
+
+  function handlePrev(event: Event) {
+    event.preventDefault();
+
+    const s = slider();
+    if (!s) return;
+
+    s.prev();
+  }
+
+  function handleNext(event: Event) {
+    event.preventDefault();
+
+    const s = slider();
+
+    if (!s) return;
+    s.next();
+  }
+
+  return html`
+    <button
+      class="keen-arrow keen-arrow--left"
+      disabled=${() => slider() && isFirstSlide(slider())}
+      ref=${(el: HTMLElement) => (leftArrow = el)}
+      onClick=${(e: Event) => handlePrev(e)}
+      type="button"
+    >
+      <span style=${srOnly}>Previous slide</span>
+      <${Show} when=${() => leftArrowTmpl}>
+        ${() => leftArrowTmpl?.content.cloneNode(true)}
+      <//>
+    </button>
+    <button
+      class="keen-arrow keen-arrow--right"
+      ref=${(el: HTMLElement) => (rightArrow = el)}
+      disabled=${() => slider() && isLastSlide(slider())}
+      onClick=${(e: Event) => handleNext(e)}
+      type="button"
+    >
+      <span style=${srOnly}>Next slide</span>
+      <${Show} when=${() => rightArrowTmpl}>
+        ${() => rightArrowTmpl?.content.cloneNode(true)}
+      <//>
+    </button>
+  `;
+};
---
+++ packages/web-components/keen-slider-navigation-arrows/src/utils.ts
@@ -0,0 +1,69 @@
+import type { KeenSliderInstance } from "@brytdesigns/web-component-keen-slider";
+import type { ICustomElement } from "component-register";
+
+export const srOnly = `border-width:0 !important;clip:rect(1px, 1px, 1px, 1px) !important;-webkit-clip-path:inset(50%) !important;clip-path:inset(50%) !important;height:1px !important;margin-top:-1px !important;margin-bottom:-1px !important;margin-right:-1px !important;margin-left:-1px !important;overflow:hidden !important;padding-top:0 !important;padding-bottom:0 !important;padding-right:0 !important;padding-left:0 !important;position:absolute !important;width:1px !important;white-space:nowrap !important;`;
+
+export function hasMoreSlides(slider: KeenSliderInstance) {
+  return slider.track.details.maxIdx > 0;
+}
+
+export function updateArrows(leftArrow: HTMLElement, rightArrow: HTMLElement) {
+  return function (slider: KeenSliderInstance) {
+    if (!leftArrow || !rightArrow) return;
+    const isFirst = isFirstSlide(slider),
+      isLast = isLastSlide(slider);
+
+    isFirst
+      ? leftArrow.classList.add("keen-arrow--disabled")
+      : leftArrow.classList.remove("keen-arrow--disabled");
+    isFirst
+      ? leftArrow.setAttribute("disabled", "true")
+      : leftArrow.removeAttribute("disabled");
+    isLast
+      ? rightArrow.classList.add("keen-arrow--disabled")
+      : rightArrow.classList.remove("keen-arrow--disabled");
+    isLast
+      ? rightArrow.setAttribute("disabled", "true")
+      : rightArrow.removeAttribute("disabled");
+  };
+}
+
+export function isFirstSlide(slider: KeenSliderInstance) {
+  return slider.track.details.rel === 0 && !slider.options.loop;
+}
+
+export function isLastSlide(slider: KeenSliderInstance) {
+  return (
+    slider.track.details.rel === slider.track.details.maxIdx &&
+    !slider.options.loop
+  );
+}
+
+export function addHiddenStyles(element: HTMLElement) {
+  element.style.setProperty("visibility", "hidden", "important");
+  element.style.setProperty("opacity", "0", "important");
+}
+
+export function addVisibleStyles(element: HTMLElement) {
+  //@ts-ignore
+  element.style.display = null;
+  //@ts-ignore
+  element.style.visibility = null;
+  //@ts-ignore
+  element.style.opacity = null;
+}
+
+type Options = {
+  element: HTMLElement & ICustomElement;
+};
+
+export function initializeClasses<Props>(
+  ComponentType = (props: Props, options: Options) => {}
+) {
+  return (props: Props, options: Options) => {
+    const { element } = options;
+    element.classList.add("keen-arrows");
+    addHiddenStyles(element);
+    return ComponentType(props, options);
+  };
+}
